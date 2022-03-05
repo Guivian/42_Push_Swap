719fb909d73689bec53a657c024509e0a707d289
Guivian <luisguilherme.viana1@gmail.com>
2022-03-05 19:59:43 +0000

    No idea what I'm doing

---
+++ push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbarbosa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/05 19:16:01 by lbarbosa          #+#    #+#             */
+/*   Updated: 2022/03/05 19:44:56 by lbarbosa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include "libft/libft.h"
+# include "ft_printf/ft_printf.h"
+
+typedef struct s_stack
+{
+	int				n;
+	int				position;
+	struct s_stack	*next;
+}t_stack;
+
+#endif
